2ed492883a973f6e3b23a66585a737585cb83f62
emilyk <emily.g.kruse@gmail.com>
2022-04-09 15:24:48 -0400

    test and build manager object

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,17 @@
+const { test } = require("@jest/globals");
+const Manager = require("../lib/Manager");
+
+test("creates a Manager object", () => {
+  const manager = new Manager("Jared", 1, "jared@fakemail.com", 200);
+
+  expect(manager.name).toBe("Jared");
+  expect(manager.id).toEqual(expect.any(Number));
+  expect(manager.email).toEqual(expect.stringContaining("@fakemail.com"));
+  expect(manager.officeNumber).toEqual(expect.any(Number));
+})
+
+test("get's manager's role, overrides employee's role", () => {
+  const manager = new Manager("Jared", 1, "jared@fakemail.com", 200);
+
+  expect(manager.getRole()).toEqual(expect.stringContaining("Manager"));
+})
