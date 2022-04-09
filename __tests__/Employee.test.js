2f56efefdd605c2049e36e50c78aab015fe40f51
emilyk <emily.g.kruse@gmail.com>
2022-04-09 14:08:38 -0400

    test and build employee methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,4 @@
+const { test, expect } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
 test("creates an Employee object", () => {
@@ -6,4 +7,18 @@ test("creates an Employee object", () => {
   expect(employee.name).toBe("John");
   expect(employee.id).toEqual(expect.any(Number));
   expect(employee.email).toEqual(expect.stringContaining("@fakemail.com"));
+});
+
+test("get employee's name, id, and email", () => {
+  const employee = new Employee("John", 1, "john@fakemail.com");
+
+  expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+  expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+  expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+test("get employee's role", () => {
+  const employee = new Employee("John", 1, "john@fakemail.com");
+
+  expect(employee.getRole()).toEqual(expect.stringContaining("Employee"));
 })
