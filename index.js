ac5e9c5e669bdb1aef96ae67e4537ff3f1d07832
emilyk <emily.g.kruse@gmail.com>
2022-04-10 14:24:46 -0400

    prompt questions for engineers and interns

--- index.js
+++ index.js
@@ -32,16 +32,12 @@ const promptManager = () => {
       name: "managerId",
       message: "Please enter the team manager's employee ID number. (Required)",
       validate: mIdInput => {
-        if (!mIdInput) {
+        if (mIdInput) {
+          return true;
+        }
+        else {
           console.log("Please enter the manager's ID!");
           return false;
-        }
-        else if (typeof mIdInput !== "number") {
-          console.log("The manager's ID must be a number!")
-          return false;
-        }
-        else if (mIdInput) {
-          return true;
         }
       }
     },
@@ -64,18 +60,118 @@ const promptManager = () => {
       name: "managerOfficeNum",
       message: "Please enter the team manager's office number. (Required)",
       validate: mOfficeNumInput => {
-        if (!mOfficeNumInput) {
+        if (mOfficeNumInput) {
+          return true;
+        }
+        else {
           console.log("Please enter the manager's office number!");
           return false;
         }
-        else if (typeof mOfficeNumInput !== "number") {
-          console.log("The manager's office number must be a number!")
-          return false;
-        }
-        else if (mOfficeNumInput) {
+      }
+    }
+  ]);
+}
+
+const promptEngineer = () => {
+  return inquirer.prompt ([
+    {
+      type: "input",
+      name: "engName",
+      message: "What is the engineer's name? (Required)",
+      validate: eNameInput => {
+        if (eNameInput) {
           return true;
         }
+        else {
+          console.log("Please enter the engineer's name!");
+          return false;
+        }
       }
+    },
+    {
+      type: "input",
+      name: "engId",
+      message: "Please enter the engineer's employee ID number. (Required)",
+      validate: eIdInput => {
+        if (eIdInput) {
+          return true;
+        }
+        else {
+          console.log("Please enter the engineer's ID!");
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "engEmail",
+      message: "What is the engineer's email address? (Required)",
+      validate: eEmailInput => {
+        if (eEmailInput) {
+          return true;
+        }
+        else {
+          console.log("Please enter the engineer's email!");
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "engGithub",
+      message: "What is the engineer's GitHub username?"
+    }
+  ])
+}
+
+const promptIntern = () => {
+  return inquirer.prompt ([
+    {
+      type: "input",
+      name: "internName",
+      message: "What is the intern's name? (Required)",
+      validate: iNameInput => {
+        if (iNameInput) {
+          return true;
+        }
+        else {
+          console.log("Please enter the intern's name!");
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "internId",
+      message: "Please enter the intern's employee ID number. (Required)",
+      validate: iIdInput => {
+        if (iIdInput) {
+          return true;
+        }
+        else {
+          console.log("Please enter the intern's ID!");
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "internEmail",
+      message: "What is the intern's email address? (Required)",
+      validate: iEmailInput => {
+        if (iEmailInput) {
+          return true;
+        }
+        else {
+          console.log("Please enter the intern's email!");
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "internSchool",
+      message: "What is the name of the school the intern attends?"
     }
   ])
 }
